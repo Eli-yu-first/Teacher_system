2a61149478b665cf3c452fb57d4ba814167b584d
sky-max-hub <1404644784@qq.com>
2020-10-29 17:30:35 +0800

    添加自动生成试卷

---
+++ src/main/java/com/hnust/view/paper/AutoPaperView.java
@@ -0,0 +1,13 @@
+package com.hnust.view.paper;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+
+/**
+ * 创建人：zhangRuWang
+ * 创建时间：2020/10/29
+ * 描述：
+ */
+@FXMLView(value = "/fxml/paper/autoPaper.fxml")
+public class AutoPaperView extends AbstractFxmlView {
+}
