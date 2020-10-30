d2a860b9991a8be87e4fbbbf4332efb28d8ed7bc
Sunshine-miao <910630676@qq.com>
2020-10-30 11:17:31 +0800

    。。。。。

--- src/main/java/com/hnust/controller/paper/GeneratePaperController.java
+++ src/main/java/com/hnust/controller/paper/GeneratePaperController.java
@@ -1,6 +1,8 @@
 package com.hnust.controller.paper;
 
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
@@ -13,9 +15,20 @@ import java.util.ResourceBundle;
 
 @FXMLController
 public class GeneratePaperController implements Initializable {
-
+    @FXML
+    HBox container;
+    @FXML
+    HBox line1;
+    @FXML
+    HBox line2;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        container.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                line1.setPrefWidth((double)newValue*0.15);
+                line2.setPrefWidth((double)newValue*0.15);
+            }
+        });
     }
 }
