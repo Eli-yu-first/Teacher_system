0c99bb0af7a2e483a5020573b1925d80f23b3a5b
王月阳 <1757908889@qq.com>
2020-10-30 09:11:38 +0800

    10.30早步骤标题样式

--- src/main/java/com/hnust/view/paper/AutoPaperView.java
+++ src/main/java/com/hnust/view/paper/AutoPaperView.java
@@ -8,6 +8,6 @@ import de.felixroske.jfxsupport.FXMLView;
  * 创建时间：2020/10/29
  * 描述：
  */
-@FXMLView(value = "/fxml/paper/autoPaper.fxml")
+@FXMLView(value = "/fxml/paper/autoPaper.fxml",css = {"/css/main.css","/css/autoPater.css"})
 public class AutoPaperView extends AbstractFxmlView {
 }
