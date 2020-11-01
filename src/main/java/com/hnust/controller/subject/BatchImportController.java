7c73804c33cc22ef319682c41914417dafc2e284
xiaolong124 <1914707213@qq.com>
2020-11-01 10:04:10 +0800

    提交成功

--- src/main/java/com/hnust/controller/subject/BatchImportController.java
+++ src/main/java/com/hnust/controller/subject/BatchImportController.java
@@ -1,24 +1,38 @@
 package com.hnust.controller.subject;
 
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 
-import java.awt.*;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @FXMLController
 public class BatchImportController implements Initializable {
+    private ObservableList playListFiles =FXCollections.observableArrayList();
     @FXML
     AnchorPane container;
-    @FXML
-    Button btn1;
-    @FXML
-    Button btn2;
+   // @FXML
+   // Button btn1;
+   // @FXML
+    //Button btn2;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
+    public void chooseFile(ActionEvent event) {
+        FileChooser fileChooser=new FileChooser();
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Text Files", "*.txt");
+        fileChooser.getExtensionFilters().add(filter);
+        File file = fileChooser.showOpenDialog(null);
+    }
+    public void downFile(){
+        System.out.println("hello world!");
+    }
+    
 }
