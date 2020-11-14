dbeca9574b67f3db71e419a2b36778a130deaeb0
xiaolong124 <1914707213@qq.com>
2020-11-14 15:00:50 +0800

    提交成功

---
+++ src/main/java/com/hnust/mock/DataList.java
@@ -0,0 +1,54 @@
+package com.hnust.mock;
+
+import com.hnust.domain.Data;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class DataList {
+    public static ObservableList<Data> initData() {
+        ObservableList<Data> list = FXCollections.observableArrayList();
+        Data d1 = new Data(1, "aaaaaaaaaaaaaaa", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d2 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d3 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d4 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d5 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d6 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d7 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d8 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d9 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d10 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d11 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d12 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d13 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        Data d14 = new Data(1, "a", "a", "a", "a", "a",
+                "a", "a", "a", "a","a","a");
+        list.add(d1);
+        list.add(d2);
+        list.add(d3);
+        list.add(d4);
+        list.add(d5);
+        list.add(d6);
+        list.add(d7);
+        list.add(d8);
+        list.add(d9);
+        list.add(d10);
+        list.add(d11);
+        list.add(d12);
+        list.add(d13);
+        list.add(d14);
+        return list;
+    }
+}
