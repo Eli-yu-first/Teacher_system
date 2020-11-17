2b8b845ddaa8262f6ef1b0903ed0fd8505649928
27915 <123456>
2020-11-17 19:01:49 +0800

    试卷列表

--- src/main/java/com/hnust/controller/paper/PaperListController.java
+++ src/main/java/com/hnust/controller/paper/PaperListController.java
@@ -1,15 +1,16 @@
 package com.hnust.controller.paper;
 
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.beans.property.SimpleStringProperty;
 import com.hnust.domain.Data;
 
 import com.hnust.mock.DataList;
@@ -18,9 +19,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.util.Callback;
+import lombok.val;
 
-import javax.security.auth.Subject;
-import javax.swing.text.TabableView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,6 +29,10 @@ public class PaperListController implements Initializable {
     ObservableList<Data> list=DataList.initData();
     @FXML
     TableView tableView;
+    @FXML
+    TableColumn columnChoice;
+//    @FXML
+//    private TableColumn<Data,Boolean> columnChoice;
     @FXML
     TableColumn columnSubject;
     @FXML
@@ -39,14 +43,75 @@ public class PaperListController implements Initializable {
     TableColumn columnOperate;
     @FXML
     Pagination page;
-//    @FXML
-//    Button btn;
     @FXML
     TextField toPage;
+    SimpleBooleanProperty flag=new SimpleBooleanProperty(false);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tableView.setItems(list);
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);//表格自适应
+        CheckBox checkBox=new CheckBox();
+        //为多选框设置双向绑定
+        checkBox.selectedProperty().bindBidirectional(flag);
+        //全选/取消全选
+        checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                TableColumn tableColumn= (TableColumn) tableView.getColumns().get(0);
+                for (Object Data: tableView.getItems()) {
+                    Data data = (Data) Data;
+                    data.setChecked(newValue);
+                }
+            }
+        });
+        columnChoice.setGraphic(checkBox);//重新设置表头格式
+        columnChoice.setSortable(false);//排序功能关闭
+        columnChoice.setCellValueFactory(new PropertyValueFactory<Data,Boolean>("checked"));
+        columnChoice.setCellFactory(new Callback<TableColumn<Data,Boolean>, TableCell<Data, Boolean>>() {
+            @Override
+            public TableCell<Data, Boolean> call(TableColumn<Data, Boolean> param) {
+                TableCell<Data,Boolean> cell=new TableCell<Data,Boolean>(){
+                    @Override
+                    protected void updateItem(Boolean item, boolean empty) {
+                        final TableCell cell=this;
+                        super.updateItem(item, empty);
+                        if(empty==false){
+                            HBox hBox=new HBox();
+                            hBox.setAlignment(Pos.CENTER);
+                            CheckBox checkBox=new CheckBox();
+                            checkBox.setSelected(item);
+                            //给CheckBox添加变化事件
+                            //TODO
+                            checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                                @Override
+                                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                                    Data data= (Data) cell.getTableRow().getItem();
+                                    data.setChecked(newValue);
+                                    int check=1;
+                                    for(Object Data:tableView.getItems()){
+                                        Data fdata=(Data) Data;
+                                        if(fdata.isChecked()==false){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                    if(check==0){
+                                        flag.set(false);
+                                    }else{
+                                        flag.set(true);
+                                    }
+                                    System.out.println(cell.getTableRow().getItem().toString());
+                                    System.out.println("CheckBox.Index: "+cell.getIndex());
+                                }
+                            });
+                            hBox.getChildren().add(checkBox);
+                            this.setGraphic(hBox);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
         columnSubject.setCellValueFactory(new PropertyValueFactory<Data,String>("Subject"));
         columnPerson.setCellValueFactory(new PropertyValueFactory<Data,String>("Person"));
         columnTime.setCellValueFactory(new PropertyValueFactory<Data,String>("Time"));
@@ -64,6 +129,14 @@ public class PaperListController implements Initializable {
                             Hyperlink h2=new Hyperlink("预览");
                             Hyperlink h3=new Hyperlink("设为样卷");
                             Hyperlink h1=new Hyperlink("删除");
+                            h1.setOnAction(new EventHandler<ActionEvent>() {
+                                @Override
+                                public void handle(ActionEvent event) {
+                                    val remove=list.remove(getIndex());
+                                    System.out.println(remove);
+                                    tableView.refresh();
+                                }
+                            });
                             h1.getStyleClass().add("color");
                             hBox.getChildren().addAll(h2,h3,h1);
                             this.setGraphic(hBox);
@@ -75,7 +148,9 @@ public class PaperListController implements Initializable {
                 return cell;
             }
         });
-//        tableView.getColumns().addAll(columnSubject,columnPerson,columnTime,columnOperate);添加表头
+        columnChoice.setPrefWidth(20);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);//表格自适应
+        tableView.setTableMenuButtonVisible(true);
         toPage.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
@@ -86,11 +161,10 @@ public class PaperListController implements Initializable {
                 }
             }
         });
-//        page.currentPageIndexProperty().addListener(new ChangeListener<Number>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-//                System.out.println("Current Page : "+(newValue.intValue()+1));
-//            }
-//        });
     }
+
+    public void delete(ActionEvent actionEvent) {
+
+    }
+
 }
--- src/main/java/com/hnust/domain/Data.java
+++ src/main/java/com/hnust/domain/Data.java
@@ -1,15 +1,20 @@
 package com.hnust.domain;
 
+import com.sun.deploy.cache.BaseLocalApplicationProperties;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
 public class Data {
     private SimpleStringProperty Subject=new SimpleStringProperty();
     private SimpleStringProperty Person=new SimpleStringProperty();
     private SimpleStringProperty Time=new SimpleStringProperty();
+    private SimpleBooleanProperty checked=new SimpleBooleanProperty();
     public Data(String Subject,String Person,String Time){
         this.Subject.set(Subject);
         this.Person.set(Person);
         this.Time.set(Time);
+        this.checked.set(false);
     }
 
     public String getSubject(){
@@ -36,13 +41,17 @@ public class Data {
 
     public void setTime(String Time){this.Time.set(Time);}
 
-//    public String getOperate(){
-//        return Operate.get();
-//    }
-//
-//    public SimpleStringProperty OperateProperty(){return Operate;}
-//
-//    public void setOperate(String Operate){this.Operate.set(Operate);}
+    public boolean isChecked(){
+        return checked.get();
+    }
+
+    public SimpleBooleanProperty checkProperty(){
+        return checked;
+    }
+
+    public void setChecked(boolean checked){
+        this.checked.set(checked);
+    }
 
     @Override
     public String toString(){
@@ -50,6 +59,7 @@ public class Data {
                 "Subject="+Subject+
                 "Person="+Person+
                 "Time="+Time+
+                ",checked="+checked+
                 "}";
     }
 }
